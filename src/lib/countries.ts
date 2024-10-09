cc78b48b768b194e3eccdd45425bd4657c8b3042
meglerhagen <Christer.hagen@gmail.com>
2024-10-09 07:26:37 +0200

    update Address, working with countries

---
+++ src/lib/countries.ts
@@ -0,0 +1,14 @@
+const countries = [
+  { code: "NO", name: "Norge" },
+  { code: "SE", name: "Sverige" },
+  { code: "DK", name: "Danmark" },
+  { code: "FI", name: "Finland" },
+  { code: "IS", name: "Island" },
+  { code: "DE", name: "Tyskland" },
+  { code: "FR", name: "Frankrike" },
+  { code: "GB", name: "Storbritannia" },
+  { code: "US", name: "USA" },
+  // Add more countries as needed
+];
+
+export default countries;
