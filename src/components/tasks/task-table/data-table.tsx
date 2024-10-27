e8342ff3e01975cedb4bd1b0caca424d4f90f26f
meglerhagen <Christer.hagen@gmail.com>
2024-10-27 12:36:42 +0100

    working table

---
+++ src/components/tasks/task-table/data-table.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { DataTablePagination } from "@/components/company/company-table/data-table-pagination";
+import { DataTableToolbar } from "./data-table-toolbar";
+import { getAllTasks } from "@/actions/tasks/get-all-tasks";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { getUserTasks } from "@/actions/tasks/get-user-tasks";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  initialData: TData[];
+  initialTotalCount: number;
+}
+
+export function TaskTable<TData, TValue>({
+  columns,
+  initialData,
+  initialTotalCount,
+}: DataTableProps<TData, TValue>) {
+  const [data, setData] = React.useState(initialData);
+  const [totalCount, setTotalCount] = React.useState(initialTotalCount);
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const updateData = React.useCallback(
+    (rowIndex: number, columnId: string, value: unknown) => {
+      setData((old) =>
+        old.map((row, index) => {
+          if (index === rowIndex) {
+            return {
+              ...old[rowIndex]!,
+              [columnId]: value,
+            };
+          }
+          return row;
+        })
+      );
+    },
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+      columnVisibility,
+      rowSelection,
+      columnFilters,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    pageCount: Math.ceil(totalCount / 10), // Assuming 10 items per page
+    meta: {
+      updateData,
+    },
+  });
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const pageIndex = table.getState().pagination.pageIndex;
+      const pageSize = table.getState().pagination.pageSize;
+      const { data: newData, totalCount: newTotalCount } = await getUserTasks(
+        pageSize,
+        pageIndex + 1
+      );
+      if (newData) {
+        setData(newData as TData[]);
+        setTotalCount(newTotalCount);
+      }
+    }
+    fetchData();
+  }, [
+    table.getState().pagination.pageIndex,
+    table.getState().pagination.pageSize,
+  ]);
+
+  return (
+    <div className="space-y-4 h-full flex flex-col">
+      <DataTableToolbar table={table} />
+      <ScrollArea className="flex-grow rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell
+                      key={cell.id}
+                      className="p-0.5 px-2 border w-min"
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Ingen resultater.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+      <DataTablePagination table={table} />
+    </div>
+  );
+}
